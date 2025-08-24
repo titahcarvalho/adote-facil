c5da46e0c6b53d1baffc8b9d072c3c86102ff48f
João Pedro Lobo <JoaoLoboIT@hotmail.com>
2025-08-24 11:11:38 -0300

    tests cypress

---
+++ frontend/cypress/e2e/testHistoria2.cy.js
@@ -0,0 +1,12 @@
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("http://localhost:3000/login");
+    cy.get('[name="email"]').type("joao@joao.com");
+    cy.get('[name="password"]').type("cabuloso");
+    cy.get(".sc-ea747762-0").click();
+    cy.get(
+      '.sc-26506e6-5 > .sc-c7cdb42d-0 > [href="/area_logada/animais_disponiveis"] > .sc-c7cdb42d-1'
+    ).click();
+    cy.get('a > .sc-ea747762-0').click();
+  });
+});
